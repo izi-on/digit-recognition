dd4865df54b3341740743019a654db18b780ce73
Hristo Sandev <sandevh@gmail.com>
2021-05-15 12:26:53 -0400

    Improve accuracy of the predictions

--- script.py
+++ script.py
@@ -9,13 +9,15 @@ x_test = tf.keras.utils.normalize(x_test, axis = 1)
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-model.add(tf.keras.layers.Dense(units=128, activation='sigmoid'))
-model.add(tf.keras.layers.Dense(units=128, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+model.add(tf.keras.layers.Dense(units=128, activation='relu'))
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=4)
 
-accuracy, loss = model.evaluate(x_test, y_test)
+loss, accuracy = model.evaluate(x_test, y_test)
 print("Accuracy: " + str(accuracy))
 print("Loss: " + str(loss))
 
